9c50b359b9a4d1ca7d8191748cb841e723810924
thedark1337 <1337alexpham@gmail.com>
2013-04-29 01:14:18 -0500

    background 5

--- IFZenhanced.js
+++ IFZenhanced.js
@@ -14,7 +14,7 @@ var meshkaEnhancedModel = Class.extend({
     init: function(){
         $('#room-wheel').hide()
         $('head').append('<style type="text/css" id="meshka-css">'
-            + 'html{background: url("http://i.imgur.com/bhv3Ay1.jpg") no-repeat scroll center top #000000;}'
+            + 'html{background: url("http://i.imgur.com/vIjULyr.jpg") no-repeat scroll center top #000000;}'
             + '#button-lobby { background-image: url("http://i.imgur.com/DNiULH5.png");}'
             + 'body {color:#66FFFF;}'
             + '#current-dj-value {color:#66FFFF;}'
