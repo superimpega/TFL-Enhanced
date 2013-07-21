db2a9082a847f1b4004cc1c71333c1446e0b4373
thedark1337 <1337alexpham@gmail.com>
2013-07-21 08:03:49 -0500

    Huge Rewrite and version bump

--- TFLenhanced.js
+++ TFLenhanced.js
@@ -4,18 +4,17 @@ String.prototype.equalsIgnoreCase = function(other) {
     return this.toLowerCase() === other.toLowerCase();
 };
 var plugCubed,
-_roomElements = RoomUser.audience.roomElements
-var TFLEnhancedModel = Class.extend({
+TFLEnhancedModel = require('app/base/Class').extend({
     version: {
         major: 1,
-        minor: 2,
-        patch: 1
+        minor: 9,
+        patch: 9
     },
     init: function(){
+        var Lang = require('lang/Lang');
         setTimeout($.proxy(this.initCSS,this), 1500)
         $('#time-remaining-value').attr('style','color: rgb(102, 255, 255);')
-         setTimeout(function(){RoomUser.audience.roomElements = []; RoomUser.redraw();},500);
-        var words = {
+                var words = {
             // Syntax: 'Search word' : 'Replace word',
             "Points" : "Points",
             "Now Playing" : "Now Playing",
@@ -70,53 +69,12 @@ var TFLEnhancedModel = Class.extend({
                 }
             }
         }
-        this.proxy = {
+                this.proxy = {
             onChat: $.proxy(this.onChat, this)
         };
-        API.addEventListener(API.CHAT,this.proxy.onChat)
-        ChatModel.chatCommand = function (a) {
-            var b;
-            if ("/help" == a) return a = {
-                    type: "update"
-            }, a.message =
-                Lang.chat.help, this.receive(a), !0;
-            if ("/strobe on" == a) {log(Models.user.data.username + ' hit the strobe light!'); return RoomUser.audience.strobeMode(true), !0;};
-            if ("/strobe off" == a) return RoomUser.audience.strobeMode(false), !0;
-            if ("/rave on" == a) {log(Models.user.data.username + ' turned the lights down!'); return RoomUser.audience.lightsOut(true), !0;};
-            if ("/rave off" == a) return RoomUser.audience.lightsOut(false), !0;
-            if ("/close" == a) return TFLEnhanced.close(), !0;
-            if ("/users" == a) return UserListOverlay.show(), !0;
-            if ("/hd on" == a) return Playback.setHD(!0), !0;
-            if ("/hd off" == a) return Playback.setHD(!1), !0;
-            if ("/chat big" == a) return Chat.expand(), !0;
-            if ("/chat small" == a) return Chat.collapse(), !0;
-            if ("/afk" == a) return Models.user.changeStatus(1), !0;
-            if ("/back" == a) return Models.user.changeStatus(0), !0;
-            if (0 == a.indexOf("/ts ")) return b = a.split(" ").pop(), DB.settings.chatTS = "12" == b ? 12 : "24" == b ? 24 : !1, this.dispatchEvent("timestampUpdate", {
-                    value: DB.settings.chatTS
-                }),
-            DB.saveSettings(), !0;
-            if (0 == a.indexOf("/cap ")) {
-                if (a = parseInt(a.split(" ").pop()), 0 < a && 201 > a) return RoomUser.audience.gridData.avatarCap = a, RoomUser.redraw(), DB.settings.avatarcap = a, DB.saveSettings(), log(Lang.messages.cap.split("%COUNT%").join("" + a)), !0
-            } else {
-                if ("/cleanup" == a) return DB.reset(), Dialog.alert(Lang.alerts.updateMessage, $.proxy(Utils.forceRefresh, Utils), Lang.alerts.update, !0), !0;
-                if ("/stream on" == a) DB.settings.streamDisabled = !1, DB.saveSettings(), Playback.media && Playback.play(Playback.media,
-                        Playback.mediaStartTime), b = "Video/audio streaming enabled.";
-                else if ("/stream off" == a) DB.settings.streamDisabled = !0, DB.saveSettings(), Playback.stop(), b = "<strong>Video/audio streaming has been stopped.</strong> Type <em>/stream on</em> to enable again.";
-                else {
-                    if ("/clear" == a) return this.dispatchEvent("chatClear"), _gaq.push(["_trackEvent", "Chat", "Clear", Models.room.data.id]), !0;
-                    Models.room.ambassadors[Models.user.data.id] ? "/fixbooth" == a && (new ModerationBoothCleanupService, b = "Fixing Booth") : Models.room.admins[Models.user.data.id] &&
-                        ("/fixbooth" == a ? (new ModerationBoothCleanupService, b = "Fixing Booth") : 0 == a.indexOf("/audience ") ? (a = parseInt(a.split(" ").pop()), 0 < a ? (RoomUser.testAddAvatar(a), b = "Adding " + a + " fake avatars to audience") : (RoomUser.clear(), RoomUser.setAudience(Models.room.getAudience()), RoomUser.setDJs(Models.room.getDJs()), b = "Cleared fake avatars from audience")) : 0 == a.indexOf("/ping ") ? (DB.settings.showPings = "/ping on" == a ? !0 : !1, DB.saveSettings(), b = "Ping messages are " + (DB.settings.showPings ? "on" : "off")) : 0 == a.indexOf("/speed ") &&
-                        (b = parseInt(a.split(" ").pop()), animSpeed = 0 < b ? b : 83, b = "Setting animation speed to " + animSpeed))
-                }
-            }
-            return b ? (a = {
-                type: "system"
-            }, a.message = b, this.receive(a), !0) : !1
-        }
-        Models.chat.chatCommand = ChatModel.chatCommand
-        log('<span style="color:#FF0000"><i>Running TFL Enhanced version ' + this.version.major + '.' + this.version.minor + '.' + this.version.patch + '</i></span>');
-        log('<span style="color:#FFFF00">Join our facebook group </span>: http://goo.gl/OKI4h')
+        API.on(API.CHAT,this.proxy.onChat)
+        API.chatLog('<span style="color:#FF0000"><i>Running TFL Enhanced version ' + this.version.major + '.' + this.version.minor + '.' + this.version.patch + '</i></span>');
+        API.chatLog('<span style="color:#FFFF00">Join our facebook group </span>: http://goo.gl/OKI4h')
         if (plugCubed == undefined) $.getScript("http://tatdk.github.io/plugCubed/compiled/plugCubed.min.js")
 
     },
@@ -154,47 +112,9 @@ var TFLEnhancedModel = Class.extend({
         Lang.messages.follow = "%NAME% is now your fan!"
         Lang.rollover.becomeFan = "Become a fan"
         Lang.chat.help = "<strong>Chat Commands:</strong><br/>/em &nbsp; <em>Emote</em><br/>/me &nbsp; <em>Emote</em><br/>/clear &nbsp; <em>Clear Chat History</em><br/>/cap # &nbsp; <em>Limits the number of avatars rendered (1-200)</em><br/>/ts # &nbsp; <em>Chat timestamps (12, 24, 0)</em>"
-        API.removeEventListener(API.CHAT,this.proxy.onChat)
-        RoomUser.audience.roomElements = _roomElements;
-        setTimeout(function(){RoomUser.redraw();},500);
+        API.off(API.CHAT,this.proxy.onChat)
         if(plugCubed != undefined) plugCubed.close();
         plugCubed = undefined
-        ChatModel.chatCommand = function (a) {
-            var b;
-            if ("/help" == a) return a = {
-                    type: "update"
-            }, a.message =
-                Lang.chat.help, this.receive(a), !0;
-            if ("/users" == a) return UserListOverlay.show(), !0;
-            if ("/hd on" == a) return Playback.setHD(!0), !0;
-            if ("/hd off" == a) return Playback.setHD(!1), !0;
-            if ("/chat big" == a) return Chat.expand(), !0;
-            if ("/chat small" == a) return Chat.collapse(), !0;
-            if ("/afk" == a) return Models.user.changeStatus(1), !0;
-            if ("/back" == a) return Models.user.changeStatus(0), !0;
-            if (0 == a.indexOf("/ts ")) return b = a.split(" ").pop(), DB.settings.chatTS = "12" == b ? 12 : "24" == b ? 24 : !1, this.dispatchEvent("timestampUpdate", {
-                    value: DB.settings.chatTS
-                }),
-            DB.saveSettings(), !0;
-            if (0 == a.indexOf("/cap ")) {
-                if (a = parseInt(a.split(" ").pop()), 0 < a && 201 > a) return RoomUser.audience.gridData.avatarCap = a, RoomUser.redraw(), DB.settings.avatarcap = a, DB.saveSettings(), log(Lang.messages.cap.split("%COUNT%").join("" + a)), !0
-            } else {
-                if ("/cleanup" == a) return DB.reset(), Dialog.alert(Lang.alerts.updateMessage, $.proxy(Utils.forceRefresh, Utils), Lang.alerts.update, !0), !0;
-                if ("/stream on" == a) DB.settings.streamDisabled = !1, DB.saveSettings(), Playback.media && Playback.play(Playback.media,
-                        Playback.mediaStartTime), b = "Video/audio streaming enabled.";
-                else if ("/stream off" == a) DB.settings.streamDisabled = !0, DB.saveSettings(), Playback.stop(), b = "<strong>Video/audio streaming has been stopped.</strong> Type <em>/stream on</em> to enable again.";
-                else {
-                    if ("/clear" == a) return this.dispatchEvent("chatClear"), _gaq.push(["_trackEvent", "Chat", "Clear", Models.room.data.id]), !0;
-                    Models.room.ambassadors[Models.user.data.id] ? "/fixbooth" == a && (new ModerationBoothCleanupService, b = "Fixing Booth") : Models.room.admins[Models.user.data.id] &&
-                        ("/fixbooth" == a ? (new ModerationBoothCleanupService, b = "Fixing Booth") : 0 == a.indexOf("/audience ") ? (a = parseInt(a.split(" ").pop()), 0 < a ? (RoomUser.testAddAvatar(a), b = "Adding " + a + " fake avatars to audience") : (RoomUser.clear(), RoomUser.setAudience(Models.room.getAudience()), RoomUser.setDJs(Models.room.getDJs()), b = "Cleared fake avatars from audience")) : 0 == a.indexOf("/ping ") ? (DB.settings.showPings = "/ping on" == a ? !0 : !1, DB.saveSettings(), b = "Ping messages are " + (DB.settings.showPings ? "on" : "off")) : 0 == a.indexOf("/speed ") &&
-                        (b = parseInt(a.split(" ").pop()), animSpeed = 0 < b ? b : 83, b = "Setting animation speed to " + animSpeed))
-                }
-            }
-            return b ? (a = {
-                type: "system"
-            }, a.message = b, this.receive(a), !0) : !1
-        }
-        Models.chat.chatCommand = ChatModel.chatCommand
     },
     initCSS: function() {
         $('#room-wheel').css('background','url("https://github.com/Colgate/TFL-Enhanced/raw/master/extras/TFL.gif")');
@@ -262,16 +182,17 @@ var TFLEnhancedModel = Class.extend({
         + '</style>');
 },
     onChat: function(data) {
-        if (data.type == 'message' && (Models.room.data.staff[data.fromID] > 2 || data.fromID == "50aeb077877b9217e2fbff00") && data.message.indexOf('!strobe on') === 0) {
-            log(data.from + ' hit the strobe light!');
-            RoomUser.audience.strobeMode(true);
-        } else if (data.type == 'message' && (Models.room.data.staff[data.fromID] > 2 || data.fromID == "50aeb077877b9217e2fbff00") && data.message.indexOf('!strobe off') === 0) {
-            RoomUser.audience.strobeMode(false);
-        } else if (data.type == 'message' && (Models.room.data.staff[data.fromID] > 2 || data.fromID == "50aeb077877b9217e2fbff00") && data.message.indexOf('!rave on') === 0) {
-            log(data.from + ' turned the lights down!');
-            RoomUser.audience.lightsOut(true)
-        } else if (data.type == 'message' && (Models.room.data.staff[data.fromID] > 2 || data.fromID == "50aeb077877b9217e2fbff00") && data.message.indexOf('!rave off') === 0) {
-            RoomUser.audience.lightsOut(false)
+        var  AudienceView = require ('app/views/room/AudienceView');
+        if (data.type == 'message' && (API.hasPermission(data.fromID, API.ROLE.MANAGER)  || data.fromID == "50aeb077877b9217e2fbff00") && data.message.indexOf('!strobe on') === 0) {
+            API.chatLog(data.from + ' hit the strobe light!');
+           AudienceView.strobeMode('true');
+        } else if (data.type == 'message' && (API.hasPermission(data.fromID, API.ROLE.MANAGER)|| data.fromID == "50aeb077877b9217e2fbff00") && data.message.indexOf('!strobe off') === 0) {
+            AudienceView.strobeMode();
+        } else if (data.type == 'message' && (API.hasPermission(data.fromID, API.ROLE.MANAGER)  || data.fromID == "50aeb077877b9217e2fbff00") && data.message.indexOf('!rave on') === 0) {
+            API.chatLog(data.from + ' turned the lights down!');
+             AudienceView.lightsOut('true');
+        } else if (data.type == 'message' && (API.hasPermission(data.fromID, API.ROLE.MANAGER)  || data.fromID == "50aeb077877b9217e2fbff00") && data.message.indexOf('!rave off') === 0) {
+            AudienceView.lightsOut();
         }
     }
 });
